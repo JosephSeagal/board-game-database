7357eeef79ebbd4eb518bf74b7e13bda69515e63
Clara Shao <cs3834@drexel.edu>
2025-11-17 19:28:04 -0500

    add create and insert table definitions

---
+++ group_team_data.sql
@@ -0,0 +1,11 @@
+INSERT INTO group_team (groupid, group_name, age_limit, budget) VALUES 
+(1, 'Members Only', NULL, 1000),
+(2, 'The Circle', NULL, 1000),
+(3, 'When Is Dinner', NULL, 100),
+(4, 'Are We There Yet', NULL, 199),
+(5, 'Same Genes', NULL, 399),
+(6, 'Full House', NULL, 399),
+(7, 'No Parents Allowed', NULL, 590),
+(8, 'Family Meeting', NULL, NULL),
+(9, 'We Have the Same Allergies', NULL, NULL),
+(10, 'Parental Control Password', NULL, NULL);
---
+++ tables.sql
@@ -0,0 +1,215 @@
+\c boardgame
+
+-- use this to clear any existing tables to reinsert fresh data
+-- you'll need to add a DROP TABLE for every table you add
+DROP TABLE IF EXISTS designer CASCADE;
+DROP TABLE IF EXISTS artist CASCADE;
+DROP TABLE IF EXISTS publisher CASCADE;
+DROP TABLE IF EXISTS single_user CASCADE;
+DROP TABLE IF EXISTS group_team CASCADE;
+DROP TABLE IF EXISTS in_group CASCADE;
+DROP TABLE IF EXISTS boardgame CASCADE;
+DROP TABLE IF EXISTS genre CASCADE;
+DROP TABLE IF EXISTS mechanic CASCADE;
+DROP TABLE IF EXISTS game_genre CASCADE;
+DROP TABLE IF EXISTS game_mechanic CASCADE;
+DROP TABLE IF EXISTS designs CASCADE;
+DROP TABLE IF EXISTS makes_art CASCADE;
+DROP TABLE IF EXISTS publishes CASCADE;
+DROP TABLE IF EXISTS searches_for CASCADE;
+DROP TABLE IF EXISTS user_favorite_game CASCADE;
+DROP TABLE IF EXISTS group_favorite_game CASCADE;
+DROP TABLE IF EXISTS user_preferred_genre CASCADE;
+DROP TABLE IF EXISTS group_preferred_genre CASCADE;
+DROP TABLE IF EXISTS user_preferred_mechanic CASCADE;
+DROP TABLE IF EXISTS group_preferred_mechanic CASCADE;
+
+
+-- Designer
+CREATE TABLE Designer (
+    designerid	INT,				-- unique ID number for this designer 
+    name		VARCHAR(200),		-- full name 
+    PRIMARY KEY (designerid)
+);
+
+-- Artist
+CREATE TABLE Artist (
+	artistid    INT,
+    name        VARCHAR(200),
+    PRIMARY KEY (artistid)
+);
+
+-- Publisher
+CREATE TABLE Publisher (
+	publisherid	INT,
+	name		VARCHAR(200),  
+	PRIMARY KEY (publisherid)
+);
+
+-- User
+CREATE TABLE Single_User (
+    userid	    INT,				-- unique ID number for this user 
+    name	    VARCHAR(200),		-- username 
+    age	        INT,
+    budget	    DECIMAL(5, 2),
+    PRIMARY KEY (userid)
+);
+
+-- Group
+CREATE TABLE Group_Team (
+    groupid     INT,				-- unique ID number for this group 
+    group_name  VARCHAR(200),		-- group name 
+    age_limit	INT,
+    budget      INT,
+    PRIMARY KEY (groupid)
+);
+
+-- InGroup
+CREATE TABLE In_Group (
+    userid      INT,				-- foreign key 
+    groupid     INT,				-- foreign key 
+    PRIMARY KEY (userid, groupid),
+    FOREIGN KEY (userid) REFERENCES Single_User (userid),
+    FOREIGN KEY (groupid) REFERENCES Group_Team (groupid)
+);
+
+-- BoardGame
+CREATE TABLE BoardGame (
+    gameid          INT,				-- unique ID number for this game 
+    title           VARCHAR(100),		-- official title of the board game 
+    description     VARCHAR(200),
+    release_year    INT,
+    min_age         INT,
+    complexity	    DECIMAL(3, 1),		-- game complexity/weight rating (1=simple, 5=very complex), range from 1.01 -> 4.82
+    min_players	    INT,
+    max_players	    INT,
+    min_playtime	INT,                -- minimum playing time in minutes 
+    max_playtime    INT,                -- maximum playing time in minutes 
+    avg_rating  	DECIMAL(4, 3),		-- range from 6.46 -> 9.16
+    price		    DECIMAL(5, 2),		-- range from 7.2 -> 979.98
+    url		        VARCHAR(200),
+    PRIMARY KEY     (gameid)
+);
+
+-- Genre
+CREATE TABLE Genre (
+	genreid     INT,
+	name        VARCHAR(200),
+	PRIMARY KEY (genreid)
+);
+
+-- Mechanic
+CREATE TABLE Mechanic (
+	mechanicid  INT,
+	name        VARCHAR(200),
+	PRIMARY KEY (mechanicid)
+);
+
+-- GameGenre
+CREATE TABLE Game_Genre (
+	gameid      INT,
+	genreid     INT,
+	PRIMARY KEY (gameid, genreid),
+	FOREIGN KEY (gameid) REFERENCES BoardGame (gameid),
+	FOREIGN KEY (genreid) REFERENCES Genre (genreid)
+);
+
+-- GameMechanic
+CREATE TABLE Game_Mechanic (
+	gameid      INT,
+	mechanicid  INT,
+	PRIMARY KEY (gameid, mechanicid),
+	FOREIGN KEY (gameid) REFERENCES BoardGame (gameid),
+	FOREIGN KEY (mechanicid) REFERENCES Mechanic (mechanicid)
+);
+
+-- Designs
+CREATE TABLE Designs (
+	designerid  INT,
+	gameid      INT,
+	PRIMARY KEY (designerid, gameid),
+	FOREIGN KEY (designerid)
+	REFERENCES  Designer (designerid),
+	FOREIGN KEY (gameid) REFERENCES BoardGame (gameid)
+);
+
+-- MakesArt
+CREATE TABLE Makes_Art (
+	artistid    INT,
+	gameid      INT,
+	PRIMARY KEY (artistid, gameid),
+	FOREIGN KEY (artistid) REFERENCES Artist (artistid),
+	FOREIGN KEY (gameid) REFERENCES BoardGame (gameid)
+);
+
+-- Publishes
+CREATE TABLE Publishes (
+	publisherid INT,
+	gameid      INT,
+	PRIMARY KEY (publisherid, gameid),
+	FOREIGN KEY (publisherid) REFERENCES Publisher (publisherid),
+	FOREIGN KEY (gameid) REFERENCES BoardGame (gameid)
+);
+
+-- SearchesFor
+CREATE TABLE Searches_For (  
+	userid      INT,
+    gameid      INT,
+	PRIMARY KEY (userid, gameid),
+	FOREIGN KEY (userid) REFERENCES Single_User (userid),
+	FOREIGN KEY (gameid) REFERENCES BoardGame (gameid)
+);
+
+-- UserFavoriteGame
+CREATE TABLE User_Favorite_Game (
+    userid      INT,
+    gameid      INT,
+    PRIMARY KEY (userid, gameid),
+    FOREIGN KEY (userid) REFERENCES Single_User (userid),
+    FOREIGN KEY (gameid) REFERENCES BoardGame (gameid)
+);
+
+-- GroupFavoriteGame
+CREATE TABLE Group_Favorite_Game (
+	groupid     INT,
+	gameid      INT,
+	PRIMARY KEY (groupid, gameid),
+	FOREIGN KEY (groupid) REFERENCES Group_Team (groupid),
+	FOREIGN KEY (gameid) REFERENCES BoardGame (gameid)
+);
+
+-- UserPreferredGenre
+CREATE TABLE User_Preferred_Genre (
+	userid      INT,
+	genreid     INT,
+	PRIMARY KEY (userid, genreid),
+	FOREIGN KEY (userid) REFERENCES Single_User (userid),
+	FOREIGN KEY (genreid) REFERENCES Genre (genreid)
+);
+
+-- UserPreferredMechanic
+CREATE TABLE User_Preferred_Mechanic (
+	userid      INT,
+	mechanicid  INT,
+	PRIMARY KEY (userid, mechanicid),
+	FOREIGN KEY (userid) REFERENCES Single_User (userid),
+	FOREIGN KEY (mechanicid) REFERENCES Mechanic (mechanicid)
+);
+
+-- GroupPreferredGenre
+CREATE TABLE Group_Preferred_Genre (
+	groupid     INT,
+	genreid     INT,
+	PRIMARY KEY (groupid, genreid),
+	FOREIGN KEY (groupid) REFERENCES Group_Team (groupid),
+	FOREIGN KEY (genreid) REFERENCES Genre (genreid)
+);
+
+-- GroupPreferredMechanic
+CREATE TABLE Group_Preferred_Mechanic (
+	groupid     INT,
+	mechanicid  INT,
+	PRIMARY KEY (groupid, mechanicid),
+	FOREIGN KEY (groupid) REFERENCES Group_Team (groupid),
+	FOREIGN KEY (mechanicid) REFERENCES Mechanic (mechanicid)
+);
